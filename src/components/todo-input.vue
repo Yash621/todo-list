776d950628671e404de7e4f56b1945a79a6a8ce0
John-Weak <tusharbali11@gmail.com>
2021-04-03 11:28:17 +0530

    get gud fix

--- src/components/todo-input.vue
+++ src/components/todo-input.vue
@@ -1,10 +1,9 @@
-/* eslint-disable vue/no-parsing-error */ /* eslint-disable vue/valid-v-bind */
 <template>
   <div>
     <h1>hello my bakchod friends</h1>
-    // eslint-disable-next-line vue/valid-v-bind // eslint-disable-next-line
-    vue/no-parsing-error
-    <div id="jkjdfkj" :class="d-flex flex-column ml-${toShowProfile() ? '350' : '60'}">{{ a }}</div>
+    <div v-bind:class="`${isRed() ? 'redText' : 'greenText'}`">
+      {{ a }}
+    </div>
     <fieldset>
       <input v-model="a" />
     </fieldset>
@@ -18,7 +17,6 @@ import { Component, Vue } from "vue-property-decorator";
 export default class TodoInput extends Vue {
   a = "";
 
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   isRed() {
     if (this.a.length > 10) {
       return true;
